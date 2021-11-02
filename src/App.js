a7c63b62cc6c9e68221c845d54f43afb5dfaa17f
Johann Eugen <jekneto@hotmail.com>
2021-11-02 16:59:01 +0100

    now we have the button of setcustomamount

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { deposit, reset, withdraw } from "./store/balance/actions";
 import { selectBalance } from "./store/balance/selectors";
+import { useState } from "react";
 
 function App() {
   // const [balance, setBalance] = useState(0);
   const balance = useSelector(selectBalance);
   const dispatch = useDispatch();
+
+  const [customAmount, setCustomAmount] = useState("");
 
   return (
     <div className="App">
@@ -35,6 +38,24 @@ function App() {
           }}
         >
           Reset
+        </button>
+      </div>
+      <div>
+        <label>Deposit Custom Amount</label>
+        <input
+          type="number"
+          value={customAmount}
+          onChange={(e) => {
+            setCustomAmount(parseInt(e.target.value));
+          }}
+        />
+        <button
+          onClick={() => {
+            dispatch(deposit(customAmount));
+            setCustomAmount(0);
+          }}
+        >
+          submit
         </button>
       </div>
     </div>
--- src/store/balance/selectors.js
+++ src/store/balance/selectors.js
@@ -4,3 +4,5 @@ export const selectBalance = (reduxState) => reduxState.balance.amount;
 export const selectWithdraw = (reduxState) => reduxState.balance.amount;
 
 export const selecResete = (reduxState) => reduxState.balance.amount;
+
+export const depositAmount = (reduxState) => reduxState.balance.amount;
